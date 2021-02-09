9033e2de4c70917b77650c7619b2d48d6d9d3969
Christoph Rissner <resi@rissner.net>
2021-02-09 22:06:21 +0100

    env update (ota, apps)

--- src/env.sh
+++ src/env.sh
@@ -7,7 +7,7 @@ DEVICE_LIST='serranoltexx,bacon'
 RELEASE_TYPE='UNOFFICIAL'
 BRANCH_NAME='lineage-18.1'
 
-OTA_URL=''
+OTA_URL='https://android.rissner.net/api'
 INCLUDE_PROPRIETARY=true
 
 SIGN_BUILDS=true
@@ -23,6 +23,8 @@ mkdir -p $KEYS_DIR $ZIP_DIR $SRC_DIR $LOGS_DIR
 
 WITH_SU=true
 SIGNATURE_SPOOFING="restricted"
+CUSTOM_PACKAGES="GmsCore GsfProxy FakeStore MozillaNlpBackend NominatimNlpBackend com.google.android.maps.jar FDroid FDroidPrivilegedExtension"
+
 
 USE_CCACHE=1
 ANDROID_JACK_VM_ARGS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4G"
@@ -33,4 +35,4 @@ DELETE_OLD_LOGS=3
 BUILD_OVERLAY=false
 LOCAL_MIRROR=false
 CLEAN_OUTDIR=false
-CLEAN_AFTER_BUILD=true
+CLEAN_AFTER_BUILD=false
